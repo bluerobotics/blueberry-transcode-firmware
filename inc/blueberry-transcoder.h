93e4b278f9fca0c427d12750df68f601bfe4ea28
Kenneth MacCallum <kenneth@offshoredesigns.ca>
2024-12-15 16:05:16 -0800

    starting function prototypes

--- inc/blueberry-transcoder.h
+++ inc/blueberry-transcoder.h
@@ -24,7 +24,8 @@ THE SOFTWARE.
 //*******************************************************************************************
 //Includes
 //*******************************************************************************************
-
+#include <stdint.h>
+#include <stdbool.h>
 //*******************************************************************************************
 //Defines
 //*******************************************************************************************
@@ -32,6 +33,15 @@ THE SOFTWARE.
 //*******************************************************************************************
 //Types
 //*******************************************************************************************
+typedef struct {
+    uint8_t* buffer;//a pointer to an array of bytes (i.e. the buffer)
+    uint32_t start;//the index of the first byte of the packet
+    uint32_t length;//the length of the packet in 4-byte words
+    uint32_t bufferLength;//the length of the buffer, assuming it's circular
+} Bb;
+
+typedef uint32_t BbBlock //this is really an index into the buffer, although it is assumed to be linear, even if the buffer wraps partway through the packet. If this value is i and the packet length is n, then 0 <= i < n.
+tyepdef uint32_t BbArray
 
 //*******************************************************************************************
 //Variables
@@ -40,6 +50,55 @@ THE SOFTWARE.
 //*******************************************************************************************
 //Function Prototypes
 //*******************************************************************************************
+/**
+ * gets a single byte value from a block
+ */
+uint8_t getBbUint8(Bb* buf, BbBlock p, uint32_t i);
+/**
+ * sets a single byte value in a block
+ */
+void setBbUint8(Bb* buf, BbBlock p, uint32_t i, uint8_t v);
+
+int8_t getBbInt8(Bb* buf, BbBlock p, uint32_t i);
+void setBbInt8(Bb* buf, BbBlock p, uint32_t i, int8_t v);
+
+uint16_t getBbUint8(Bb* buf, BbBlock p, uint32_t i);
+void setBbUint8(Bb* buf, BbBlock p, uint32_t i, uint16_t v);
+
+int16_t getBbInt8(Bb* buf, BbBlock p, uint32_t i);
+void setBbInt8(Bb* buf, BbBlock p, uint32_t i, int16_t v);
+
+uint32_t getBbUint8(Bb* buf, BbBlock p, uint32_t i);
+void setBbUint8(Bb* buf, BbBlock p, uint32_t i, uint32_t v);
+
+int32_t getBbInt8(Bb* buf, BbBlock p, uint32_t i);
+void setBbInt8(Bb* buf, BbBlock p, uint32_t i, int32_t v);
+
+float getBbUint8(Bb* buf, BbBlock p, uint32_t i);
+void setBbUint8(Bb* buf, BbBlock p, uint32_t i, float v);
+
+float getBbInt8(Bb* buf, BbBlock p, uint32_t i);
+void setBbInt8(Bb* buf, BbBlock p, uint32_t i, float v);
+
+float getBbBool(Bb* buf, BbBlock p, uint32_t i, uint32_t bitNum);
+void setBbBool(Bb* buf, BbBlock p, uint32_t i, float v, uint32_t bitNum, bool v);
+
+uint32_t bbWrap(uint32_t i, uint32_t n);
+
+/**
+ * Computes the index of the next block in a packet.
+ * Note that this does not wrap the index - it leaves it as if the buffer is linear.
+ * This is for convenience of testing for bounds
+ * @return the next block in the paycket
+ */
+Block getBbNextBlock(Bb* buf, BbBlock p);
+
+bool isBbNextBlock(Bb* buf, BbBlock p);
+
+
+
+
+
 
 //*******************************************************************************************
 //Code
