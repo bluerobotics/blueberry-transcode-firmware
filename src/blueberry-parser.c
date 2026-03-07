4ded934cd9204aa0133c3821b515b511e402ebb8
Kenneth MacCallum <kenneth@offshoredesigns.ca>
2026-03-07 11:22:38 -0800

    moved status led code

--- src/blueberry-parser.c
+++ src/blueberry-parser.c
@@ -90,7 +90,6 @@ static void registerProcessor(Processors * ps, uint32_t key, BbProcessor p);
 void initBbParser(void){
 	m_parsers.num = 0;
 	m_builders.num = 0;
-	statusLedPacketReceived();
 	registerUdpListener(BB_UDP_PORT, processBlueberryPacket, false);
 	setEthernetPort(BB_UDP_PORT, BB_UDP_PORT);
 }
